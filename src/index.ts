9252830014699dafaf97f0793c96b5248195a252
Bryan DeGrendel <bryan@degrendel.com>
2024-06-21 16:25:05 -0400

    Add infrastructure for endpoints
    Next step is Kafka and wiring things.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,42 @@
 import { Kafka, KafkaConfig } from 'kafkajs';
+import express, { Express, Request, Response } from 'express';
+import session from 'express-session';
+import createMemoryStore from 'memorystore';
+import expressWs from 'express-ws';
+import dotenv from 'dotenv';
+import { ChatMessage } from 'elusivebot-schema';
+
+dotenv.config();
 
 const kafkaConfig: KafkaConfig = {
-  clientId: 'http',
-  brokers: ['kafka1:9092', 'kafka2:9092'],
+  clientId: process.env.KAFKA_CLIENT_ID || 'http',
+  brokers:  process.env.KAFKA_BOOTSTRAP!.split(','),
 };
 const kafka = new Kafka(kafkaConfig);
 
-console.log("Hello world!");
+const server: Express = express();
+const MemoryStore = createMemoryStore(session);
+const ExpressWs = expressWs(server);
+const app = ExpressWs.app;
+
+app.use(session({
+    cookie: { maxAge: 86400000, },
+    store: new MemoryStore({
+      checkPeriod: 86400000 // prune expired entries every 24h
+    }),
+    saveUninitialized: false,
+    resave: false,
+    secret: process.env.SESSION_SECRET!,
+}));
+
+app.get('/', function(req, res, next){
+  res.end();
+});
+
+app.ws('/', function(ws, req) {
+  ws.on('message', function(msg) {
+    console.log(msg);
+  });
+});
+
+app.listen(process.env.HTTP_PORT || 8080)
