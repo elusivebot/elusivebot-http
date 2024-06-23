aa5362eac1d495015499908200352a079da2685c
Bryan DeGrendel <bryan@degrendel.com>
2024-06-23 15:33:53 -0400

    Removed sessions
    To greatly simplify things, just have the state of the websocket
    track the start and end of an ELB session.  In the future, might make
    this more sophisticated, and will decide then how it works.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 import { Kafka, KafkaConfig } from 'kafkajs';
 import express, { Express, Request, Response } from 'express';
-import session from 'express-session';
-import createMemoryStore from 'memorystore';
 import expressWs from 'express-ws';
 import dotenv from 'dotenv';
 import { ChatMessage } from 'elusivebot-schema';
@@ -15,25 +13,14 @@ const kafkaConfig: KafkaConfig = {
 const kafka = new Kafka(kafkaConfig);
 
 const server: Express = express();
-const MemoryStore = createMemoryStore(session);
 const ExpressWs = expressWs(server);
 const app = ExpressWs.app;
-
-app.use(session({
-    cookie: { maxAge: 86400000, },
-    store: new MemoryStore({
-      checkPeriod: 86400000 // prune expired entries every 24h
-    }),
-    saveUninitialized: false,
-    resave: false,
-    secret: process.env.SESSION_SECRET!,
-}));
 
 app.get('/', function(req, res, next){
   res.end();
 });
 
-app.ws('/', function(ws, req) {
+app.ws('/chat', function(ws, req) {
   ws.on('message', function(msg) {
     console.log(msg);
   });
